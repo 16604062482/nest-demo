5c8c4535afd2977a305010fc6b443a523f8919ec
zhangningle <1160030724@qq.com>
2023-12-13 10:21:38 +0800

    feat: api

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,34 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 
-@Controller('person')
+@Controller('api/person')
 export class PersonController {
   constructor(private readonly personService: PersonService) {}
 
+  // form urlencoded
   @Post()
-  create(@Body() createPersonDto: CreatePersonDto) {
-    return this.personService.create(createPersonDto);
+  body(@Body() createPersonDto: CreatePersonDto) {
+    return `received: ${JSON.stringify(createPersonDto)}`;
   }
 
-  @Get()
-  findAll() {
-    return this.personService.findAll();
+  // query
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age: number) {
+    return `received:name = ${name},age = ${age}`;
   }
 
+  // url param
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.personService.findOne(+id);
+  urlParam(@Param('id') id: string) {
+    debugger;
+    return `received:id=${id}`;
   }
+  // findOne(@Param('id') id: string) {
+  //   return this.personService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
-    return this.personService.update(+id, updatePersonDto);
-  }
+  // @Post()
+  // create(@Body() createPersonDto: CreatePersonDto) {
+  //   return this.personService.create(createPersonDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.personService.remove(+id);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.personService.findAll();
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+  //   return this.personService.update(+id, updatePersonDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.personService.remove(+id);
+  // }
 }
